21f0d7ed83a40c6e162c3659146eb24b17d5d496
toabr <c3to@web.de>
2019-04-11 11:55:20 +0200

    Updates

---
+++ precache-manifest.d83b00c3dca7554f4d857b3de5650bf3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "31a53f4fb72e40542ce5",
+    "url": "/wiki/static/js/main.482ae026.chunk.js"
+  },
+  {
+    "revision": "2117087dc213cefe10b0",
+    "url": "/wiki/static/js/runtime~main.bbc02b0e.js"
+  },
+  {
+    "revision": "96a741518374a0502809",
+    "url": "/wiki/static/css/2.e34b7ea5.chunk.css"
+  },
+  {
+    "revision": "96a741518374a0502809",
+    "url": "/wiki/static/js/2.0c26ae30.chunk.js"
+  },
+  {
+    "revision": "215143158a2c304310ec731118667be0",
+    "url": "/wiki/static/media/logo.21514315.jpg"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/wiki/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/wiki/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/wiki/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/wiki/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/wiki/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/wiki/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/wiki/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/wiki/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/wiki/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/wiki/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/wiki/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/wiki/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/wiki/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/wiki/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/wiki/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/wiki/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/wiki/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/wiki/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/wiki/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/wiki/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/wiki/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/wiki/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/wiki/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/wiki/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "da61080cae682e44326a4bee1d57fbe6",
+    "url": "/wiki/index.html"
+  }
+];
